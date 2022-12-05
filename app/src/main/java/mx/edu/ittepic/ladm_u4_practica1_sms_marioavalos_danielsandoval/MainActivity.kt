832b63a15569831785f4e93ee33e85c9112f060d
Dany Sandoval <dasandovalal@ittepic.edu.mx>
2022-12-05 12:17:22 -0700

    Los SMS se envian

--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_sms_marioavalos_danielsandoval/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_sms_marioavalos_danielsandoval/MainActivity.kt
@@ -1,11 +1,58 @@
 package mx.edu.ittepic.ladm_u4_practica1_sms_marioavalos_danielsandoval
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val siPermiso = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnEnviar.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.SEND_SMS), siPermiso)
+            }else {
+                enviarSMS(txtDestino, txtSMS)
+            }
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == siPermiso){
+            enviarSMS(txtDestino,txtSMS)
+        }
+    }
+
+    fun enviarSMS(destino:EditText,msg:EditText){
+        try {
+            var telefono = destino.text.toString()
+            var mensaje = msg.text.toString()
+            var msj = SmsManager.getDefault()
+            msj.sendTextMessage("$telefono", null, "$mensaje",
+                null, null)
+            Toast.makeText(this,"SE ENVIO EL MENSAJE",Toast.LENGTH_SHORT).show()
+        }catch (e:Exception){
+            Log.d("~Error","${e.message}")
+        }
+    }
+
+
+
 }
